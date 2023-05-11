a4fefcd59a026b2ee1a260b12808a18ad699bbbb
Jean-Francois Lavoie <atmailjfl@gmail.com>
2023-05-11 10:59:15 -0400

    Fixed batch file commands

--- CapsisWebAPI/app_update.bat
+++ CapsisWebAPI/app_update.bat
@@ -5,11 +5,14 @@ cd ..
 git pull 
 cd CapsisWebAPI
 
-REM Deploy Capsis installation
-pushd %CAPSISWEBAPI_CAPSIS_PATH%
-ant installer-export
-robocopy bin\Release\net6.0\win-x64\publish c:\inetpub\%1\capsis /E 
-popd
+REM Deploy Capsis installation - the ant script messes up the current directory so we store it into a variable
+set "CUR_DIR=%~dp0"
+REM create a capsis/jar subfolder that is needed by the ant script to operate
+mkdir %CAPSISWEBAPI_CAPSIS_PATH%\jar
+call %CAPSISWEBAPI_CAPSIS_PATH%\ant.bat installer-export
+robocopy %CAPSISWEBAPI_CAPSIS_PATH%\export c:\inetpub\%1\capsis /E 
+cd %CUR_DIR%
+mkdir c:\inetpub\%1\capsis\data
 
 REM build app
 dotnet publish -c Release -r win-x64 
@@ -18,7 +21,7 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 %windir%\system32\inetsrv\appcmd stop apppool /apppool.name:%1
 if %errorlevel% neq 0 if %errorlevel% neq 1062 exit /b %errorlevel%
 
-robocopy bin\Release\net5.0\win-x64\publish c:\inetpub\%1 /E 
+robocopy bin\Release\net6.0\win-x64\publish c:\inetpub\%1 /E 
 if %errorlevel% geq 9 exit /b %errorlevel%
 
 %windir%\system32\inetsrv\appcmd start apppool /apppool.name:%1
