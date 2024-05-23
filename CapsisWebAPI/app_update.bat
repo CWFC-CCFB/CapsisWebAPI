671976b4fbcee7da415bdeead5674bffe569271d
donenrique24 <mathieu.fortin.re@gmail.com>
2024-05-23 16:24:55 -0400

    Deployement batch file now adapted to service strategy. App now listens to 5300.

--- CapsisWebAPI/app_update.bat
+++ CapsisWebAPI/app_update.bat
@@ -10,6 +10,10 @@ set "CUR_DIR=%~dp0"
 REM create a capsis/jar subfolder that is needed by the ant script to operate
 mkdir %CAPSISWEBAPI_CAPSIS_PATH%\jar
 call %CAPSISWEBAPI_CAPSIS_PATH%\ant.bat installer-export -Dmodules="quebecmrnf/**,artemis/**,artemis2014/**"
+
+sc stop %1
+if %errorlevel% neq 0 if %errorlevel% neq 1062 exit /b %errorlevel%
+
 robocopy %CAPSISWEBAPI_CAPSIS_PATH%\export c:\inetpub\%1\capsis /E /purge
 cd %CUR_DIR%
 mkdir c:\inetpub\%1\capsis\data
@@ -18,12 +22,9 @@ REM build app
 dotnet publish -c Release -r win-x64 
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-%windir%\system32\inetsrv\appcmd stop apppool /apppool.name:%1
-if %errorlevel% neq 0 if %errorlevel% neq 1062 exit /b %errorlevel%
-
 robocopy bin\Release\net6.0\win-x64\publish c:\inetpub\%1 /E 
 if %errorlevel% geq 9 exit /b %errorlevel%
 
-%windir%\system32\inetsrv\appcmd start apppool /apppool.name:%1
-REM %windir%\system32\inetsrv\appcmd start site /site.name:%1
+sc start %1
+if %errorlevel% neq 0 if %errorlevel% neq 1062 exit /b %errorlevel%
 
